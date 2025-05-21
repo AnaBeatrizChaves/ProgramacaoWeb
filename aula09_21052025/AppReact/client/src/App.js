ab3de26ed2abede9adebb04b82d2c76a7f473382
Ana Beatriz <anabeatrizc76@gmail.com>
2025-05-21 20:59:13 -0300

    feat aula09_21052025 conexão com o banco

---
+++ aula09_21052025/AppReact/client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Axios from "axios";
+import CadastroAluno from './components/CadastroAluno';
+
+function App() {
+  return (
+    <div className="App">
+      <CadastroAluno></CadastroAluno>
+    </div>
+  );
+}
+
+export default App;
